efb2bed9d729887bb90051cdd5880eeb0029054f
Alienmario <daniel321321@gmail.com>
2023-03-07 01:31:46 +0100

    Build setup

---
+++ mp/src/build_server.bat
@@ -0,0 +1,18 @@
+@echo off
+cls
+
+IF "%MSBUILD22%"=="" (
+	echo Install build Tools for Visual Studio 2022 ^(select Desktop programming package^) and define MSBUILD22 env variable first
+	echo For example: "D:\Program Files (x86)\Microsoft Visual Studio\2022\BuildTools\MSBuild\Current\Bin\MSBuild.exe"
+	pause && exit
+)
+
+devtools\bin\vpc.exe /2013 /sdk2013ce +dedicated
+
+echo:
+echo ----------------- BUILD START -----------------
+echo:
+
+"%MSBUILD22%" game/server/server_sdk2013ce.vcxproj
+
+pause
