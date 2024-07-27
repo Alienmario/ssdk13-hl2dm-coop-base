830b5f2b08152419648e7259e7edd300ca46f2b2
Alienmario <daniel321321@gmail.com>
2024-07-27 21:06:29 +0200

    Update win build script

--- mp/src/build_server.bat
+++ mp/src/build_server.bat
@@ -4,9 +4,6 @@ SETLOCAL
 :start
 cls
 
-echo ----------------- VPC START -----------------
-echo:
-
 IF "%MSBUILD22%"=="" (
 	echo Install build Tools for Visual Studio 2022 ^(select Desktop programming package^) and define MSBUILD22 env variable first
 	echo For example: "D:\Program Files (x86)\Microsoft Visual Studio\2022\BuildTools\MSBuild\Current\Bin\MSBuild.exe"
@@ -18,17 +15,32 @@ IF "%config%"=="" (
 	set config=Release
 )
 
-devtools\bin\vpc.exe /2013 /sdk2013ce +dedicated
+echo ----------------- VPC START -----------------
+echo.
 
-echo:
+devtools\bin\vpc.exe /2013 /sdk2013ce +dedicated
+IF %ERRORLEVEL% GTR 0 goto retry
+
+echo.
 echo ----------------- BUILD START (%config%) -----------------
-echo:
+echo.
 
 : "%MSBUILD22%" raytrace/raytrace.vcxproj -p:Configuration=%config%
+: IF %ERRORLEVEL% GTR 0 goto retry
 : "%MSBUILD22%" tier1/tier1.vcxproj -p:Configuration=%config%
+: IF %ERRORLEVEL% GTR 0 goto retry
 : "%MSBUILD22%" mathlib/mathlib.vcxproj -p:Configuration=%config%
+: IF %ERRORLEVEL% GTR 0 goto retry
 "%MSBUILD22%" game/server/server_sdk2013ce.vcxproj -p:Configuration=%config%
+IF %ERRORLEVEL% GTR 0 goto retry
 
+IF NOT "%sourcesdk_server_dest%"=="" (
+	echo.
+	echo Copying server binary to "%sourcesdk_server_dest%\bin\"
+	copy /b "..\game\mod_sdk2013ce\bin\server.dll" "%sourcesdk_server_dest%\bin\"
+)
+
+:retry
 IF NOT "%2%"=="noretry" (
 	echo:
 	echo Retry?
